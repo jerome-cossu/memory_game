ec1cfc4433bdb9ce5500fd5d97d135b29ab65620
jerome-cossu <jerome.cossu@laplateforme.io>
2025-04-23 09:50:56 +0200

    🎉 feat : shuffle card in the terminal

---
+++ models/MatchCards.java
@@ -0,0 +1,78 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class MatchCards {
+    class Card {
+        String cardName;
+        ImageIcon cardImageIcon;
+
+        Card(String cardName, ImageIcon cardImageIcon) {
+            this.cardName = cardName;
+            this.cardImageIcon = cardImageIcon;
+        }
+
+        public String toString() {
+            return cardName;
+        }
+    }
+    // track cardNames
+    String[] cardlist = {
+        "darkness",
+        "double",
+        "fairy",
+        "fighting",
+        "fire",
+        "grass",
+        "lightning",
+        "metal",
+        "psychic",
+        "water"
+    };
+    
+    int rows = 4;
+    int columns = 5;
+    int cardWidth = 90;
+    int cardHeight = 128;
+
+    // create a deck of cards
+    ArrayList<Card> cardSet;
+    ImageIcon cardBackImageIcon; 
+
+
+    MatchCards() {
+        setupCards();
+        suffleCards();
+    }
+
+    void setupCards() {
+        cardSet = new ArrayList<Card>();
+        for (String cardName : cardlist) {
+            // load card image
+            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
+            ImageIcon cardImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
+
+            // create card object and add to cardSet
+            Card card = new Card(cardName, cardImageIcon);
+            cardSet.add(card);
+        }
+        cardSet.addAll(cardSet);
+
+        // load card back image
+        Image cardBackImg = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
+        cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
+    }
+    void suffleCards() {
+        // suffle the cards in cardSet
+        System.out.println(cardSet);
+        for (int i = 0; i < cardSet.size(); i++) {
+            int j = (int) (Math.random() * cardSet.size());
+            Card temp = cardSet.get(i);
+            cardSet.set(i, cardSet.get(j));
+            cardSet.set(j, temp);
+        }
+        System.out.println(cardSet);
+    }
+    
+}
